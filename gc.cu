cea5d77ade24853436823c09a9f4715c3e21f474
Michel Pelletier <pelletier.michel@gmail.com>
2015-12-22 00:51:13 -0800

    more reorg

--- gc.cu
+++ gc.cu
@@ -58,3 +58,69 @@ x_any x_gc() {
    set_val(cell, freed);
    return cell;
 }
+
+x_any c_alloc(x_any type) {
+  x_any cell;
+  if (!(cell = x_env.cell_pools->free))
+    assert(0);
+  x_env.cell_pools->free = car(cell);
+  set_cdr(cell, NULL);
+  set_car(cell, NULL);
+  type(cell) = type;
+  return cell;
+}
+
+void* x_alloc(size_t size) {
+void* result;
+  cudaMallocManaged(&result, size);
+  cudaStreamAttachMemAsync(x_env.stream, result);
+  CHECK;
+  assert(result != NULL);
+  return result;
+}
+
+char* new_name(const char* name) {
+  char *n;
+  n = (char*)malloc(strlen(name) + 1);
+  strcpy(n, name);
+  return n;
+}
+
+x_any new_cell(const char* name, x_any type) {
+  x_any cell;
+  cell = c_alloc(type);
+  if (name == NULL)
+    set_val(cell, NULL);
+  else
+    set_val(cell, new_name(name));
+  return cell;
+}
+
+x_any new_int(int64_t value) {
+  x_any cell;
+  cell = new_cell(NULL, x_env.int_);
+  set_val(cell, value);
+  return cell;
+}
+
+x_cell_pool* new_cell_pool(x_cell_pool* old) {
+  x_cell_pool* h;
+  x_any cell;
+  h = (x_cell_pool*)malloc(sizeof(x_cell_pool));
+  h->next = old;
+  cell = h->cells + X_YOUNG_CELL_POOL_SIZE - 1;
+  do
+    free_cell(h, cell);
+  while (--cell >= h->cells);
+  return h;
+}
+
+x_frame* new_frame() {
+  x_frame * f;
+  f = (x_frame*)malloc(sizeof(x_frame));
+  f->next = NULL;
+  f->prev = NULL;
+  for (int i = 0; i < X_HASH_TABLE_SIZE; i++)
+    f->names[i] = x_env.nil;
+  return f;
+}
